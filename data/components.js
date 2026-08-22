f59abbb9632822e92d2622c45d57905c8d025063
Andrea <ggandream24@gmail.com>
2026-08-22 15:23:17 -0600

    hotfix: urls in github

--- data/components.js
+++ data/components.js
@@ -1,6 +1,7 @@
+const root = new URL("../", import.meta.url);
 export const components = [
-  { page: "Home", url: `/` },
-  { page: "Button", url: `/components/Button/` },
-  { page: "ActionIcon", url: `/components/ActionIcon/` },
-  { page: "SearchButton", url: `/components/SearchButton/` },
+  { page: "Home", url: ` ${root}` },
+  { page: "Button", url: ` ${root}components/Button/` },
+  { page: "ActionIcon", url: ` ${root}components/ActionIcon/` },
+  { page: "SearchButton", url: ` ${root}components/SearchButton/` },
 ];
