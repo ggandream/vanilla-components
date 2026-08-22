8d403432569e1411eb70615048e2523bf04d72bd
Andrea <ggandream24@gmail.com>
2026-08-22 14:43:06 -0600

    hotfix: urls github

--- data/components.js
+++ data/components.js
@@ -1,11 +1,11 @@
-let host = new URL(document.location.host);
+let host = new URL(document.location.origin);
 
 if (document.location.href.includes("github")) {
-  host = new URL(document.location.host + "vanilla-components/");
+  host = new URL(document.location.origin + "vanilla-components/");
 }
 export const components = [
-  { page: "Home", url: `${host}/` },
-  { page: "Button", url: `${host}/components/Button/` },
-  { page: "ActionIcon", url: `${host}/components/ActionIcon/` },
-  { page: "SearchButton", url: `${host}/components/SearchButton/` },
+  { page: "Home", url: `${host}` },
+  { page: "Button", url: `${host}components/Button/` },
+  { page: "ActionIcon", url: `${host}components/ActionIcon/` },
+  { page: "SearchButton", url: `${host}components/SearchButton/` },
 ];
