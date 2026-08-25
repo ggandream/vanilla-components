f2f38f61a41351b7882c24c6d699a2e723215e74
Andrea <ggandream24@gmail.com>
2026-08-24 21:40:04 -0600

    wip: mobile menu

--- data/components.js
+++ data/components.js
@@ -5,4 +5,5 @@ export const components = [
   { page: "ActionIcon", url: ` ${root}components/ActionIcon/` },
   { page: "SearchButton", url: ` ${root}components/SearchButton/` },
   { page: "Accordion", url: ` ${root}components/Accordion/` },
+  { page: "NavList", url: ` ${root}components/NavList/` },
 ];
